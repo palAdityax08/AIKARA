851a1f84e2c460e2052c8d45265b64ac3045f787
Aditya <paladitya7508@gmail.com>
2025-12-28 19:58:40 +0530

    Delete unused directory

--- unused/stt.py
+++
@@ -1,9 +0,0 @@
-import whisper
-
-model = whisper.load_model("large-v2")
-
-result = model.transcribe(audio = "audios/12_Exercise 1 - Pure HTML Media Player.mp3", 
-                          language="hi",
-                          task="translate" )
-
-print(result["text"])
